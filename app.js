536eefe64b47c143c918c89ff3a9b23a8ead6323
richcoleman <coleman.richardm@gmail.com>
2018-11-01 00:55:53 -0500

    updates to portfolio

---
+++ app.js
@@ -0,0 +1,26 @@
+// const projects = {[
+//         simpsons {
+//         name: "simpsons",
+//         details: "This is a Simpsons trivia game I made in order to get accustomed to jQuery and manipulating the DOM.",
+//         githubLink: "https://github.com/RichColeman/TriviaGame",
+//         deployLink: "https://richcoleman.github.io/TriviaGame/"
+//     },
+//     gif {
+//         name: "gif",
+//         details: "This is an NBA Gif Search that was an exercise in calling APIs and manipulating the DOM.",
+//         githubLink: "https://github.com/RichColeman/gif-dont-lie",
+//         deployLink: "https://richcoleman.github.io/gif-dont-lie/"
+//     },
+
+//     name: "platypus",
+//     details: "A web application to help international travelers get information at a glance. First group project.",
+//     githubLink: "https://github.com/jQuery-jokers/Platypus",
+//     deployLink: "https://jquery-jokers.github.io/Platypus/"
+// ]}
+
+// console.log(projects[0].details);
+
+// $(".project").on(click, function () {
+//     let button = $(this).val();
+//     let newDiv = $("<div>").append(button);
+// });
